db77024ea92a1941ab2e0e4a54e7d8313a39c300
Ikhsan Fakhrudin <ihsaanef@gmail.com>
2023-06-05 13:12:16 +0700

    File Project

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+// Created by DONIHSAN
+
+let masuk = document.getElementById("login");
+let daftar = document.getElementById("Register");
+let ok = document.getElementById("btn");
+
+
+function login() {
+    masuk.style.left = "50px";
+    daftar.style.left = "450px";
+    ok.style.left = "0px";
+}
+
+function Register() {
+    masuk.style.left = "-400px";
+    daftar.style.left = "50px";
+    ok.style.left = "110px";
+}    
+
+
